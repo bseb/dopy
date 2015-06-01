298788fb9c477016889fe1005039ba673af5b8b5
Ben Sebastian <ben.sebastian@icloud.com>
2015-06-01 14:21:31 -0400

    added more functions and a Custom Exception class

--- dopy.py
+++ dopy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import json
 try:
     import requests as r
     from requests.auth import HTTPBasicAuth
@@ -7,8 +8,9 @@ except ImportError:
     print('Requests module missing,please install Requests')
 
 
-class dopy():
+class Dopy():
     APIURL = "https://api.digitalocean.com/v2"
+    jsonheaders = {'content-type': 'application/json'}
 
     def __init__(self, APIToken):
         self.APIToken = APIToken
@@ -30,17 +32,19 @@ class dopy():
         return ActionList
 
     def ListAvailableImages(self):
-        Images = r.get(self.APIURL + "/images?page=1&per_page=999&type=distribution",
-                       auth=self.BasicAuth)
+        Images = r.get(self.APIURL + "/images?page=1&per_page=999&" +
+                       "type=distribution", auth=self.BasicAuth)
         AvailableImages = Images.json()
         return AvailableImages
 
     def CreateDroplet(self, name="example", region="nyc3", size="512mb",
                       image="centos-7-0-x64", **kwargs):
-        DropParms = {"name": name, "region": region, "size": size, "image": image}
+        DropParms = {"name": name, "region": region, "size": size,
+                     "image": image}
         DropParms.update(kwargs)
         Create = r.post(self.APIURL + "/droplets", auth=self.BasicAuth,
-                        params=DropParms)
+                        headers=self.jsonheaders,
+                        data=json.dumps(DropParms))
         CreatedDroplet = Create.json()
         return CreatedDroplet
 
@@ -70,25 +74,29 @@ class dopy():
     def DestroyDroplet(self, id):
         Delete = r.delete(self.APIURL + "/droplets/" + id, auth=self.BasicAuth)
         return Delete
-    # TODO Handle Return Codes 204 is success, 404 droplet not found
 
     def ListDropletUpgrade(self):
         Upgrade = r.get(self.APIURL + "/droplet_upgrades", auth=self.BasicAuth)
         DropletUpgrades = Upgrade.json()
         return DropletUpgrades
 
-    def RebootDroplet(self, id):
-        command = {"type": "reboot"}
-        Reboot = r.post(self.APIURL + "/droplets/" + id + "/actions", params=command,
-                        auth=self.BasicAuth)
-        DropletReboot = Reboot.json()
-        return DropletReboot
-
     def DropletPowerControl(self, id, action):
+        ValidActions = ["power_cycle", "shutdown", "power_off", "power_on",
+                        "reboot"]
+        if action not in ValidActions:
+            raise DopyError("%s is not a valid action" % action)
         command = {"type": action}
-        Dpower = r.post(self.APIURL + "/droplets/" + id + "/actions", params=command,
-                        auth=self.BasicAuth)
+        Dpower = r.post(self.APIURL + "/droplets/" + id + "/actions",
+                        params=command, auth=self.BasicAuth)
         DropPower = Dpower.json()
         return DropPower
-    # Valid values for action are "power_cycle" "shutdown" "power_off" "power_on"
+    # Valid values of action are "power_cycle" "shutdown" "power_off" "power_on"
 
+    def ListKeys(self):
+        Keys = r.get(self.APIURL + "/account/keys", auth=self.BasicAuth)
+        KeyList = Keys.json()
+        return KeyList
+
+
+class DopyError(Exception):
+    pass
